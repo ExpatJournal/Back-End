7109ac3c038ea71b10d2a8c8084ceca22551a92e
Dustin <clerin+gitkraken@gmail.com>
2019-08-28 20:16:34 -0700

    heroku testing to get delete to return

---
+++ data/migrations/20190828201531_expat-change-delete-to-cascade.js
@@ -0,0 +1,93 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('users', t => {
+    t.increments();
+    t.string('username', 25)
+      .notNullable()
+      .unique();
+    t.string('displayName', 25)
+      .notNullable()
+      .unique();
+    t.string('email', 30)
+      .notNullable()
+      .unique();
+    t.string('password', 255)
+      .notNullable();
+  })
+  .createTable('journal', t => {
+    t.increments();
+    t.string('title', 255)
+      .notNullable();
+    t.integer('author_id')
+      .notNullable()
+      .unsigned()
+      .references('id')
+      .inTable('users')
+      .onDelete('CASCADE')
+      .onUpdate('CASCADE');
+    t.string('location', 255);
+    t.string('post', 21000);
+    t.biginteger('created_date')
+      .notNullable();
+    t.biginteger('updated_date')
+      .notNullable();
+  })
+  .createTable('media', t => {
+    t.increments();
+    t.string('url', 255)
+      .notNullable();
+    t.string('caption', 2100);
+    t.integer('owner_id')
+      .notNullable()
+      .unsigned()
+      .references('id')
+      .inTable('users')
+      .onDelete('CASCADE')
+      .onUpdate('CASCADE');
+  })
+  .createTable('journal_media', t => {
+    t.increments();
+    t.unique([ 'post_id', 'media_id' ]);
+    t.integer('post_id')
+      .notNullable()
+      .unsigned()
+      .references('id')
+      .inTable('journal')
+      .onDelete('CASCADE')
+      .onUpdate('CASCADE');
+    t.integer('media_id')
+      .notNullable()
+      .unsigned()
+      .references('id')
+      .inTable('media')
+      .onDelete('RESTRICT')
+      .onUpdate('CASCADE');
+  })
+  .createTable('comments', t => {
+    t.increments();
+    t.integer('post_id')
+      .notNullable()
+      .unsigned()
+      .references('id')
+      .inTable('journal')
+      .onDelete('CASCADE')
+      .onUpdate('CASCADE');
+    t.integer('author_id')
+      .notNullable()
+      .unsigned()
+      .references('id')
+      .inTable('users')
+      .onDelete('RESTRICT')
+      .onUpdate('CASCADE');
+    t.string('comment', 2100)
+      .notNullable();
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('comments')
+    .dropTableIfExists('journal_media')
+    .dropTableIfExists('media')
+    .dropTableIfExists('journal')
+    .dropTableIfExists('users');
+};
